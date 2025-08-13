57b6b7d25b0342857a475ebcf2a534089670aa40
Jawad-Ahmad-M <jdmgl2238@gmail.com>
2025-08-13 12:42:21 +0500

    first commit

---
+++ app/src/main/java/com/resubuild/app/MainActivity.java
@@ -0,0 +1,98 @@
+package com.resubuild.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.resubuild.app.databinding.ActivityMainBinding;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private final Set<Integer> visitedPages = new HashSet<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        MainPageAdapter pagerAdapter = new MainPageAdapter(this);
+        binding.viewPager.setAdapter(pagerAdapter);
+
+        // Start with minimal offscreenPageLimit
+        binding.viewPager.setOffscreenPageLimit(1);
+
+        // Track visited pages and dynamically increase offscreenPageLimit
+        binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                if (!visitedPages.contains(position)) {
+                    visitedPages.add(position);
+                    binding.viewPager.setOffscreenPageLimit(visitedPages.size());
+                }
+
+//                 Sync BottomNavigationView selection
+                switch (position) {
+                    case 0:
+                        binding.navView.setSelectedItemId(R.id.nav_details);
+                        break;
+                    case 1:
+                        binding.navView.setSelectedItemId(R.id.nav_pdf_view);
+                        break;
+                    case 2:
+                        binding.navView.setSelectedItemId(R.id.nav_ats);
+                        break;
+                    case 3:
+                        binding.navView.setSelectedItemId(R.id.nav_profile);
+                        break;
+                }
+            }
+        });
+
+        setupBottomNav();
+
+        // Restore last selected page if any
+        if (savedInstanceState != null) {
+            int lastPage = savedInstanceState.getInt("last_page", 0);
+            binding.viewPager.setCurrentItem(lastPage, false);
+            visitedPages.add(lastPage);
+            binding.viewPager.setOffscreenPageLimit(visitedPages.size());
+        }
+    }
+
+    private void setupBottomNav() {
+        binding.navView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_details) {
+                binding.viewPager.setCurrentItem(0, false);
+                return true;
+            }
+            else if (itemId == R.id.nav_pdf_view){
+                binding.viewPager.setCurrentItem(1, false);
+                return true;
+            }
+            else if (itemId == R.id.nav_ats){
+                binding.viewPager.setCurrentItem(2, false);
+                return true;
+            }
+            else if (itemId == R.id.nav_profile){
+                binding.viewPager.setCurrentItem(3, false);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("last_page", binding.viewPager.getCurrentItem());
+        super.onSaveInstanceState(outState);
+    }
+}
---
+++ app/src/main/java/com/resubuild/app/fragments/pdfViewFragment.java
@@ -0,0 +1,30 @@
+package com.resubuild.app.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.resubuild.app.R;
+
+public class pdfViewFragment extends Fragment {
+
+    public pdfViewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pdf_view, container, false);
+    }
+}
